18cf55ad96d6f2139856949d54284531f80f17d9
Nerea Rico <nerearico.tic@gmail.com>
2023-12-14 12:13:54 +0100

    1.2.2 Creation of the CurrentAccount

---
+++ NRBank/src/components/CurrentAccount.java
@@ -0,0 +1,11 @@
+//1.2.2 Creation of the CurrentAccount
+
+package components;
+
+public class CurrentAccount extends Account {
+
+	public CurrentAccount(String label, Clients client, int accountNumber) {
+		super(label, client, accountNumber);
+	}
+
+}
