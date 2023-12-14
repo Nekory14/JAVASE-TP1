82a3bd6f4098d5750ebcf2e05bd7cf70abc420f7
Nerea Rico <nerearico.tic@gmail.com>
2023-12-14 12:14:30 +0100

    1.2.2 Creation of the SavingsAccount

---
+++ NRBank/src/components/SavingsAccount.java
@@ -0,0 +1,11 @@
+//1.2.2 Creation of the SavingsAccount
+
+package components;
+
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(String label, Clients client, int accountNumber) {
+		super(label, client, accountNumber);
+	}
+
+}
